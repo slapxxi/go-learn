ca1719b483ec0ec6671430aba7daec9fd7a395fb
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-11-16 09:58:53 +0300

    Simple function

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
+	result := div(10, 0)
+
+	fmt.Println(result)
+}
+
+func div(num int, den int) int {
+	if den == 0 {
+		fmt.Println("Division by zero")
+		return 0
+	}
+
+	return num / den
 }
