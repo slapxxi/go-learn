401917329aed347384cf0b7a5f6d1e32b59b4aff
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-12-24 11:47:11 +0300

    simple request line impl

--- request/request.go
+++ request/request.go
@@ -1,6 +1,10 @@
 package request
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
 
 type Request struct {
 	RequestLine RequestLine
@@ -18,10 +22,43 @@ type Response struct {
 	Body io.ReadCloser
 }
 
+var ERROR_MALFORMED_RLINE = fmt.Errorf("malformed request line oopsie!")
+var SEPARATOR = "\r\n"
+
+func parseRequestLine(s string) (*RequestLine, string, error) {
+	idx := strings.Index(s, SEPARATOR)
+	if idx == -1 {
+		return nil, s, ERROR_MALFORMED_RLINE
+	}
+	startLine := s[:idx]
+	restOfMsg := s[idx+len(SEPARATOR):]
+	parts := strings.Split(startLine, " ")
+	if len(parts) != 3 {
+		return nil, s, ERROR_MALFORMED_RLINE
+	}
+	return &RequestLine{
+		Method:        parts[0],
+		RequestTarget: parts[1],
+		HTTPVersion:   parts[2],
+	}, restOfMsg, nil
+}
+
 func (r Request) Do() (*Response, error) {
 	return &Response{Body: io.NopCloser(nil)}, nil
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	return nil, nil
+	content, err := io.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	requestLine, _, err := parseRequestLine(string(content))
+	if err != nil {
+		return nil, err
+	}
+	return &Request{
+		RequestLine: *requestLine,
+		Headers:     nil,
+		Body:        nil,
+	}, nil
 }
--- request/request_test.go
+++ request/request_test.go
@@ -13,22 +13,22 @@ func TestRequest(t *testing.T) {
 	var err error
 
 	// Test: Good GET Request line
-	r, err = RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:4206"))
+	r, err = RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:4206\r\nUser-Agent: curl/7.81.0\r\nAccept:*/*\r\n\r\n"))
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
 	assert.Equal(t, "/", r.RequestLine.RequestTarget)
-	assert.Equal(t, "1.1", r.RequestLine.HTTPVersion)
+	assert.Equal(t, "HTTP/1.1", r.RequestLine.HTTPVersion)
 
 	// Test: Good GET Request line with path
-	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:4206"))
+	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:4206\r\nUser-Agent: curl/7.81.0\r\nAccept:*/*\r\n\r\n"))
 	require.NoError(t, err)
 	require.NotNil(t, r)
 	assert.Equal(t, "GET", r.RequestLine.Method)
 	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
-	assert.Equal(t, "1.1", r.RequestLine.HTTPVersion)
+	assert.Equal(t, "HTTP/1.1", r.RequestLine.HTTPVersion)
 
 	// Test: Invalid number of parts in request line
-	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:4206"))
+	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:4206\r\nUser-Agent: curl/7.81.0\r\nAccept:*/*\r\n\r\n"))
 	require.Error(t, err)
 }
